1781d8d984774296edeefd81c8d00e8d3a04d544
DanTetzlaff <tetzlaffdanielj@gmail.com>
2015-11-09 20:08:57 -0700

    Update travel-header.inc.php

--- project2/includes/travel-header.inc.php
+++ project2/includes/travel-header.inc.php
@@ -26,8 +26,8 @@
            </div>
            <div class="navbar-collapse collapse">
              <ul class="nav navbar-nav">
-               <li><a href="#">Home</a></li>
-               <li><a href="#about">About</a></li>
+               <li><a href="index.php">Home</a></li>
+               <li><a href="#about.php">About</a></li>
                <li><a href="#contact">Contact</a></li>
                <li class="dropdown">
                  <a href="#" class="dropdown-toggle" data-toggle="dropdown">Browse <b class="caret"></b></a>
