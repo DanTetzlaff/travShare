70815a5e4ae036df5da700008ed25d224f790038
carillem <carillem@users.noreply.github.com>
2015-11-11 16:24:07 -0700

    Update travel-header.inc.php

--- project2/includes/travel-header.inc.php
+++ project2/includes/travel-header.inc.php
@@ -22,7 +22,7 @@
               <li><a href="#"><span class="glyphicon glyphicon-user"></span> Profile</a></li>
               <li><a href="view-favorites.php"><span class="glyphicon glyphicon-star"></span> Favorites <span class="badge"><?php echo countFav(); ?></span></a></li>
 			  <li><a href="#"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
-			  <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Cart <span class="badge"><?php echo countCart(); ?></span></li>
+			  <li><a href="view-cart.php"><span class="glyphicon glyphicon-shopping-cart"></span> Cart <span class="badge"><?php echo countCart(); ?></span></li>
             </ul>         
          </div>
       </div>
