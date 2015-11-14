7830101793998ccfb35258da06121285863999d9
carillem <cmend429@mtroyal.ca>
2015-11-13 23:22:58 -0700

    added search box to search-results page
    to do: hide the rest of the page if no query has been put in

--- project2/search-results.php
+++ project2/search-results.php
@@ -28,19 +28,39 @@
 				<li><a href="index.php">Home</a></li>
 				<li class="active">Search</li>
 			</ol>
-			<!--print out search query -->
-			<h4> Results for: <i><?php print($_SESSION['searchString']); ?></i></h4>
-			<!-- print out images results as thumbnails. Hover will display the image description and img is a link to the image's single-image.php page -->
-			<div class = "panel panel-default">
-				<div class = "panel-body">
-					<?php 
-						/**
-							method found in travel-utilities.inc.php and returns thumbnails from the array of images it is given.
-							displayImagesThumbnails($searchResults);
-						**/
-					?>
+			<!-- search box for new searches -->
+			<form method = "post" class = "inline" action = "<?php echo $_SERVER['PHP_SELF']; ?>">
+				<div class = "form-group form-group-lg">
+					<div class = "col-md-6">
+						<input type="text" class="form-control input-lg" placeholder="<?php 
+						if(isset($_SESSION['searchString']) && $_SESSION['searchString'] != "") 
+						{
+							print($_SESSION['searchString']);
+						}
+						else
+						{	
+							echo 'Search for image';
+						}
+						?>" name="srch">
+					</div>
+					<button class="btn btn-default btn-lg" type="submit" name = "submit">Search</button>
 				</div>
-			</div>
+			</form>
+			<div class = "well well-md">
+				<!--print out search query -->
+				<h4> Results for: <i><?php print($_SESSION['searchString']); ?></i></h4>
+				<!-- print out images results as thumbnails. Hover will display the image description and img is a link to the image's single-image.php page -->
+				<div class = "panel panel-default">
+					<div class = "panel-body">
+						<?php 
+							/**
+								method found in travel-utilities.inc.php and returns thumbnails from the array of images it is given.
+								displayImagesThumbnails($searchResults);
+							**/
+						?>
+					</div>
+				</div>
+			</div> <!--end well -->
 		</div> <!-- end main content column -->
 	</div> <!-- end main content row -->
 </div> <!-- end main content container -->
