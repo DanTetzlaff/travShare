08a7fa7e583954f76983fd8087c8ac8528b8e71f
DanTetzlaff <tetzlaffdanielj@gmail.com>
2015-12-04 12:59:35 -0700

    Create mapJS.js

---
+++ project2/js/mapJS.js
@@ -0,0 +1,134 @@
+var styles = [
+    {
+        "featureType": "administrative",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road.local",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "visibility": "on"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#84afa3"
+            },
+            {
+                "lightness": 52
+            }
+        ]
+    },
+    {
+        "stylers": [
+            {
+                "saturation": -17
+            },
+            {
+                "gamma": 0.36
+            }
+        ]
+    },
+    {
+        "featureType": "transit.line",
+        "elementType": "geometry",
+        "stylers": [
+            {
+                "color": "#3f518c"
+            }
+        ]
+    }
+];
+
+function initMap() {
+		// set settings for the map and assign custom style to default map options
+	var mapOptions = {
+		zoom: 13,
+		center: myLatLng,
+		styles: styles
+	};
+		
+		//create map object
+	var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+	
+	// basic non-style map
+	
+	/*var map = new google.maps.Map(document.getElementById('map'), {
+		zoom: 13,
+		center: myLatLng
+	});*/
+
+		//set a marker to be placed on the map at the coordinated of the picture
+	var marker = new google.maps.Marker({
+		animation: google.maps.Animation.DROP,
+		position: myLatLng,
+		map: map,
+		title: imageT
+	});
+}
+
