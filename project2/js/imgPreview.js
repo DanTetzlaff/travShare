103cb1c219ebd07ec95b79cbb6e3e902b0112979
DanTetzlaff <tetzlaffdanielj@gmail.com>
2015-12-07 12:10:32 -0700

    Update imgPreview.js
    added documentation

--- project2/js/imgPreview.js
+++ project2/js/imgPreview.js
@@ -1,5 +1,15 @@
-window.addEventListener('load', imageHandler);
+/*
+Author: Daniel Tetzlaff
+Created: November 2015
+Version: 1.1
+	- adjusted to handle removing preview image in assignment 3 case
+Makes handlers and shows/hides image previews on a mouse hovering over a thumbnail
+*/
 
+
+window.addEventListener('load', imageHandler); // check to see if page has loaded, begin running once page is loaded
+
+// make event handlers for mouseover and mouseout events that occur on each image on the page
 function imageHandler() {
 	var images = document.querySelectorAll(".thumbImg");
 	for (i=0; i < images.length; i++) {
@@ -8,15 +18,22 @@ function imageHandler() {
 	}
 }
 
+// displays the preveiw image when mouseover event occurs
 function showImage(e) {
 	var img = document.createElement('img');
-	var pathA = this.src;
+	var pathA = this.src; // grab source from the original image
+	
+	 // break path up from original to get the file name
 	var path = "images/travel/small/" + pathA.split('/').pop();
-	img.setAttribute('src', path);
+	
+	// make new path so we can use a small image for the preview
+	img.setAttribute('src', path); 
 	img.setAttribute('class', 'bigImg');
-	this.parentNode.parentNode.appendChild(img);
+	// append this to the original image
+	this.parentNode.parentNode.appendChild(img); 
 }
 
+// remove the preview image by removing the last child appended to the image previews grandparent node
 function removeImage(e) {
 	this.parentNode.parentNode.removeChild(this.parentNode.parentNode.childNodes[this.parentNode.parentNode.childNodes.length -1]);
 }
